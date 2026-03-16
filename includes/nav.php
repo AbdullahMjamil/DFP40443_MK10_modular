d544ec0eec47f7b5f965ef6f82baeb5229ff947c
Abdullah Mohd Jamil <18DIT24F1114@Student.ptss.edu.my>
2026-03-16 13:59:44 +0800

    Finalize modular structure

---
+++ includes/nav.php
@@ -0,0 +1,5 @@
+<nav class="nav-menu">
+    <a href="index.php?menu=utama" class="nav-link <?= ($activeMenu === 'utama') ? 'active' : '' ?>">Utama</a>
+    <a href="index.php?menu=tempah" class="nav-link <?= ($activeMenu === 'tempah') ? 'active' : '' ?>">Tempah</a>
+    <a href="index.php?menu=invois" class="nav-link <?= ($activeMenu === 'invois') ? 'active' : '' ?>">Invois</a>
+</nav>
